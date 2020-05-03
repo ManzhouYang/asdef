7772ee8edc6d13ed66ec11a7987db45c95cc55a7
ManzhouYang <64312971+ManzhouYang@users.noreply.github.com>
2020-05-03 09:20:40 +0200

    Update cheeseshop.py

--- examples/cheeseshop.py
+++ examples/cheeseshop.py
@@ -1,3 +1,11 @@
-import asdef
+from asdef import cheeseshop
 while True:
-  pass
+	kinds = ['Baconburger', 'Chips(pomes frites)', 'Hamburger']
+	ft = ['cheese', 'bacon', 'potato']
+	kind = random.choice(kinds)
+	fts = random.choice(ft)
+	cheeseshop(kind,
+		   fts = 'YES')
+	s = input('(no or yes)More ')
+	if s == 'no':
+		break
