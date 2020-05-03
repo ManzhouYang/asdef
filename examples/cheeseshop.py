f5aaaed539e75513d20d510c715d3c9ef1771144
ManzhouYang <64312971+ManzhouYang@users.noreply.github.com>
2020-05-03 11:49:36 +0200

    Update cheeseshop.py

--- examples/cheeseshop.py
+++ examples/cheeseshop.py
@@ -1,18 +1,18 @@
 import asdef, random
 while True:
-  kinds = ['Baconburger', 'Hamburger', 'chips(fries)', 'ice cream', 'bacon fries']
-  kind = random.choice(kinds)
-  if kind == 'Baconburger':
-    fills = 'bacon'
-    fills2 = 'cucumber'
-    fill3 = True
-    fills3 = 'meat'
-  elif kind == 'bacon fries':
-    fills = 'bacon'
-    fills2 = 'potato'
-    fill3 = True
-    fills3 = 'ketchup'
-  elif kind == 'chips(fries)':
-    fills = 'potato'
-  asdef.cheeseshop(kind)
-  print(fills, ': YES')
+    kinds = ['Baconburger', 'Hamburger', 'chips(fries)', 'ice cream', 'bacon fries']
+    kind = random.choice(kinds)
+    if kind == 'Baconburger':
+        fills = 'bacon'
+        fills2 = 'cucumber'
+        fill3 = True
+        fills3 = 'meat'
+    elif kind == 'bacon fries':
+        fills = 'bacon'
+        fills2 = 'potato'
+        fill3 = True
+        fills3 = 'ketchup'
+    elif kind == 'chips(fries)':
+        fills = 'potato'
+    asdef.cheeseshop(kind)
+    print(fills, ': YES')
